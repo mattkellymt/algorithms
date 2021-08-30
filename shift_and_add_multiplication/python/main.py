9bcc589c1289528eabd5d3625fb81099e55593f2
Matthew Kelly <>
2021-08-29 22:15:40 -0700

    added permutations

--- shift_and_add_multiplication/python/main.py
+++ shift_and_add_multiplication/python/main.py
@@ -1,6 +1,7 @@
+from itertools import permutations
+
 def recurse(a, b):
-    if a == 1:
-        return b
+    if a == 1: return b
     result = recurse(a >> 1, b << 1)
     result += b if (a & 1) else 0
     return result
@@ -16,12 +17,11 @@ if __name__ == "__main__":
     max = 10
     valid = True
 
-    for a in range(max):
-        for b in range(max):
-            c1 = a * b
-            c2 = product(a, b)
-            
-            if c1 != c2:
-                valid = False
+    for a, b in permutations(range(max), 2):
+        c1 = a * b
+        c2 = product(a, b)
+        
+        if c1 != c2:
+            valid = False
 
     print(f"Correct: {valid}")
